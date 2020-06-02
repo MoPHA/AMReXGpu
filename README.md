f6191f638091cf03f86ea4a9e828da18211de045
Nortamo <40563680+Nortamo@users.noreply.github.com>
2020-06-02 15:21:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Examples of very basic AMReX GPU functionality 
+
+Starting with OpenACC, might try other things later
+
+## Planned (in order of implementation and difficulty)
+- Particle to Particle
+- Grid to Particle
+- Particle to grid (local only)
+- Particle to grid (including remote deposition)
+- geometric PIC (essentially line integral deposition)
---
+++ pp_main.cpp
@@ -0,0 +1,39 @@
+// MWE for particle to particle propagation
+// on GPU using amrex
+
+// Amrex
+#include <AMReX.H>
+#include <AMReX_Print.H>
+#include <AMReX_BoxArray.H>
+#include <AMReX_Geometry.H>
+#include <AMReX_RealBox.H>
+#include <AMReX_CoordSys.H>
+#include <AMReX_MultiFab.H>
+#include <AMReX_DistributionMapping.H>
+#include <AMReX_ParallelDescriptor.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_Particles.H>
+#include <AMReX_Particle.H>
+#include <AMReX_Utility.H>
+#include <AMReX_ParticleUtil.H>
+#include <AMReX_NeighborParticles.H>
+#include "AMReX_Array4.H"
+
+
+void main_main();
+
+int main(int argc, char* argv[])
+{
+
+    amrex::Initialize(argc,argv);
+    main_main();
+    amrex::Finalize();
+
+}
+void main_main()
+{
+    
+    std::array<int,3> n_cell;
+    std::array<int,3> max_grid_size;
+
+}
